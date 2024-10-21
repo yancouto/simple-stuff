c04f51f5c907e2b9796b20f642f9ed8d479c7726
Yan Couto <yancouto@gmail.com>
2024-10-21 01:27:33 -0300

    Basic example bot

--- stream_of_conciousness_bot/src/main.rs
+++ stream_of_conciousness_bot/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
